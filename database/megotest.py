3372291adb26f68852f559e105b7b2a8c0183ac3
jexbest <xxqpxx@mail.ru>
2024-11-17 17:09:37 +0300

    new bot

--- database/megotest.py
+++ database/megotest.py
@@ -1,5 +1,7 @@
-from models import  view_all_notes
+from models import  view_all_notes, filter_diary_by_date
 
 
 telegram_id = 332859984
 view_all_notes(telegram_id)
+date = "2024-10-28"
+filter_diary_by_date(telegram_id, date)
